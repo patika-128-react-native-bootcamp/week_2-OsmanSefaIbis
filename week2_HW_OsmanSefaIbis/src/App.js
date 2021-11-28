89080fb727431213b37af33ab983e6cde7977723
Osman Sefa İbis <sefa.ibis@inavitas.com>
2021-11-28 15:46:03 +0300

    The build is established, printed hello world as a starter.

---
+++ week2_HW_OsmanSefaIbis/src/App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text> Hello World...</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
